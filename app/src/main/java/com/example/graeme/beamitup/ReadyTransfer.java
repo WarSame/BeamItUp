e142897d8510759dd30e688191d40afcc923c772
WarSame <graemecliffe@gmail.com>
2017-10-23 12:19:15 -0400

    Working callback. Tag sends to other phone. Need to work on processing tag on the receiving end.

--- app/src/main/java/com/example/graeme/beamitup/ReadyTransfer.java
+++ app/src/main/java/com/example/graeme/beamitup/ReadyTransfer.java
@@ -3,6 +3,7 @@ package com.example.graeme.beamitup;
 import android.content.Intent;
 import android.nfc.FormatException;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
 import android.os.Bundle;
@@ -13,11 +14,9 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class ReadyTransfer extends Activity implements NfcAdapter.CreateNdefMessageCallback {
+import java.nio.charset.StandardCharsets;
 
-    NfcAdapter mNfcAdapter;
-    Transfer tran;
-
+public class ReadyTransfer extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,26 +31,22 @@ public class ReadyTransfer extends Activity implements NfcAdapter.CreateNdefMess
         TextView tv_amount_reason = (TextView)findViewById(R.id.tv_reason_value);
         tv_amount_reason.setText(reason);
 
-        tran = new Transfer(amount, reason);
+        Transfer tran = new Transfer(amount, reason);
 
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if (mNfcAdapter == null){
+        NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
             Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
             finish();
             return;
         }
-        mNfcAdapter.setNdefPushMessageCallback(this, ReadyTransfer.this);
+
+        NdefRecord rec = NdefRecord.createExternal("com.example.graeme.beamitup", "transfer", tran.toString().getBytes());
+        NdefMessage msg = new NdefMessage(rec);
+
+        String str = new String(msg.getRecords()[0].getPayload(), StandardCharsets.UTF_8);
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show();
+
+        mNfcAdapter.setNdefPushMessage(msg, this);
     }
 
-    public NdefMessage createNdefMessage(NfcEvent event) {
-        Toast.makeText(this, "landed", Toast.LENGTH_LONG).show();
-        try {
-            Toast.makeText(this, tran.toString(), Toast.LENGTH_LONG).show();
-            return new NdefMessage(tran.toString().getBytes());
-        } catch (FormatException e) {
-            Toast.makeText(this, "format exception", Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
-        return null;
-    }
 }
