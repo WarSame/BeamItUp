2ce9e485237c144a9d4c2df6f3683e79eb3a2e76
WarSame <graemecliffe@gmail.com>
2018-01-23 08:41:57 -0500

    Finish is working, need to test with actual eth values.

--- app/src/main/java/com/example/graeme/beamitup/request/FinishRequestActivity.java
+++ app/src/main/java/com/example/graeme/beamitup/request/FinishRequestActivity.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.graeme.beamitup.R;
+import com.example.graeme.beamitup.Session;
+import com.example.graeme.beamitup.account.Account;
 import com.example.graeme.beamitup.eth.Eth;
 import com.example.graeme.beamitup.eth.EthDbAdapter;
 import com.example.graeme.beamitup.transfer.SendTransactionTask.SendTransferResponse;
 
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
+
+import java.util.NoSuchElementException;
 
 public class FinishRequestActivity extends Activity {
 
@@ -23,21 +28,22 @@ public class FinishRequestActivity extends Activity {
 
         Intent incomingIntent = getIntent();
         Request request = (Request)incomingIntent.getSerializableExtra("request");
-        Eth eth = (Eth)incomingIntent.getSerializableExtra("eth");
         try {
-            sendTransfer(eth.getId(), request);
+            sendTransfer(request);
         } catch (Exception e) {
             e.printStackTrace();
+            finishRequestFail(request);
         }
     }
 
-    private void sendTransfer(final long ethID, final Request request) throws Exception {
-        String senderPrivateKey = getSenderPrivateKey(ethID, request.getFromAddress());
+    private void sendTransfer(final Request request) throws Exception {
+        Account account = Session.getUserDetails();
+        Eth eth = selectEthFromAccountByAddress( account, request.getFromAddress() );
+
+        String senderPrivateKey = getSenderPrivateKey(eth.getId(), request.getFromAddress());
         Credentials credentials = Credentials.create(senderPrivateKey);
 
          SendTransferResponse sendTransferResponse = transactionReceipt -> {
-            ProgressBar pbSendTransfer = (ProgressBar)findViewById(R.id.pb_send_transfer);
-            pbSendTransfer.setVisibility(View.GONE);
             if (transactionReceipt == null){
                 finishRequestFail(request);
             }
@@ -50,10 +56,36 @@ public class FinishRequestActivity extends Activity {
         task.execute(request);
     }
 
+    Eth selectEthFromAccountByAddress(Account account, String fromAddress) throws NoSuchElementException {
+        for ( Eth eth : account.getEths() ){
+            if ( eth.getAddress().equals( fromAddress ) ){
+                return eth;
+            }
+        }
+        throw new NoSuchElementException();
+    }
+
     private void finishRequestSuccess(TransactionReceipt transactionReceipt) {
+        removeProgressBar();
+        Toast.makeText(
+                this,
+                "Request from " + transactionReceipt.getTo() + " fulfilled.",
+                Toast.LENGTH_LONG
+        ).show();
     }
 
     private void finishRequestFail(Request request) {
+        removeProgressBar();
+        Toast.makeText(
+                this,
+                "Request from " + request.getFromAddress() + " not fulfilled.",
+                Toast.LENGTH_LONG
+        ).show();
+    }
+
+    private void removeProgressBar(){
+        ProgressBar pbSendTransfer = (ProgressBar)findViewById(R.id.pb_send_transfer);
+        pbSendTransfer.setVisibility(View.GONE);
     }
 
     String getSenderPrivateKey(long ethID, String senderAddress) throws Exception{
