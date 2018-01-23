99eef6f29cfac20490edde9303d84d1e17a0f1a3
WarSame <graemecliffe@gmail.com>
2018-01-22 21:41:13 -0500

    Also created a Request class.

---
+++ app/src/main/java/com/example/graeme/beamitup/request/Request.java
@@ -0,0 +1,46 @@
+package com.example.graeme.beamitup.request;
+
+import java.io.Serializable;
+
+public class Request implements Serializable {
+    private static final String TAG = "Request";
+
+    private String amount;
+    private String fromAddress;//Requestee
+    private String toAddress;//Requester
+
+    Request(String toAddress, String amount){
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+
+    public String getToAddress() {
+        return toAddress;
+    }
+
+    public void setToAddress(String toAddress) {
+        this.toAddress = toAddress;
+    }
+
+    public String getFromAddress() {
+        return fromAddress;
+    }
+
+    public void setFromAddress(String fromAddress) {
+        this.fromAddress = fromAddress;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String toString(){
+        return "Amount: "+ this.amount
+                + " fromAddress: " + this.fromAddress
+                + " toAddress: " + this.toAddress;
+    }
+}
