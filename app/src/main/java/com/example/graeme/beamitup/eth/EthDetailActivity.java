19f2ca76dc49815ea155f56d45eb8e07c18c2a02
WarSame <graemecliffe@gmail.com>
2018-05-11 19:55:14 -0400

    Minimally working eth details.

--- app/src/main/java/com/example/graeme/beamitup/eth/EthDetailActivity.java
+++ app/src/main/java/com/example/graeme/beamitup/eth/EthDetailActivity.java
@@ -2,6 +2,8 @@ package com.example.graeme.beamitup.eth;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.graeme.beamitup.R;
 
@@ -11,7 +13,15 @@ public class EthDetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eth_detail);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView tv_eth_wallet_name = findViewById(R.id.tv_eth_wallet_name);
+        EditText et_eth_nickname = findViewById(R.id.et_eth_nickname);
+        EditText et_eth_address = findViewById(R.id.et_eth_address);
+
+        Eth eth = (Eth) getIntent().getSerializableExtra("eth");
+        tv_eth_wallet_name.setText(eth.getWalletName());
+        et_eth_address.setText(eth.getAddress());
+        et_eth_nickname.setText(eth.getNickname());
     }
 
 }
