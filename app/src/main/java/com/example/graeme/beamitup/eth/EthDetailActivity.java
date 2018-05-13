591bcf61cfe20800226e36748c3fb79fb2033753
WarSame <graemecliffe@gmail.com>
2018-05-13 13:47:22 -0400

    Working eth update. Need to auto transition back to parent.

--- app/src/main/java/com/example/graeme/beamitup/eth/EthDetailActivity.java
+++ app/src/main/java/com/example/graeme/beamitup/eth/EthDetailActivity.java
@@ -2,9 +2,12 @@ package com.example.graeme.beamitup.eth;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.graeme.beamitup.BeamItUp;
 import com.example.graeme.beamitup.R;
 
 public class EthDetailActivity extends Activity {
@@ -16,12 +19,26 @@ public class EthDetailActivity extends Activity {
 
         TextView tv_eth_wallet_name = findViewById(R.id.tv_eth_wallet_name);
         EditText et_eth_nickname = findViewById(R.id.et_eth_nickname);
-        EditText et_eth_address = findViewById(R.id.et_eth_address);
+        TextView tv_eth_address = findViewById(R.id.tv_eth_address);
 
         Eth eth = (Eth) getIntent().getSerializableExtra("eth");
+
         tv_eth_wallet_name.setText(eth.getWalletName());
-        et_eth_address.setText(eth.getAddress());
+        tv_eth_address.setText(eth.getAddress());
         et_eth_nickname.setText(eth.getNickname());
+
+        Button btn_save_eth = findViewById(R.id.btn_save_eth);
+        btn_save_eth.setOnClickListener((v)->{
+            eth.setNickname(et_eth_nickname.getText().toString());
+            updateEth(eth);
+        });
+    }
+
+    private void updateEth(Eth eth) {
+        DaoSession daoSession = ((BeamItUp) getApplication()).getDaoSession();
+        EthDao ethDao = daoSession.getEthDao();
+        ethDao.update(eth);
+        Toast.makeText(this, "Eth saved", Toast.LENGTH_LONG).show();
     }
 
 }
