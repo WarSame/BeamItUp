59281b29e84348b1135903655540444c3be32711
WarSame <graemecliffe@gmail.com>
2018-05-13 21:38:18 -0400

    Notification displaying but not launchable

--- app/src/main/java/com/example/graeme/beamitup/eth_tasks/GenerateWalletService.java
+++ app/src/main/java/com/example/graeme/beamitup/eth_tasks/GenerateWalletService.java
@@ -1,12 +1,16 @@
 package com.example.graeme.beamitup.eth_tasks;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,6 +22,7 @@ import com.example.graeme.beamitup.R;
 import com.example.graeme.beamitup.eth.DaoSession;
 import com.example.graeme.beamitup.eth.Eth;
 import com.example.graeme.beamitup.eth.EthDao;
+import com.example.graeme.beamitup.eth.EthDetailActivity;
 import com.example.graeme.beamitup.wallet.EncryptedWallet;
 import com.example.graeme.beamitup.wallet.WalletHelper;
 
@@ -41,8 +46,8 @@ public class GenerateWalletService extends IntentService {
             String nickname = intent.getStringExtra("nickname");
             File walletDir = WalletHelper.getWalletDir(this);
             String walletName = WalletHelper.generateWallet(longPassword, walletDir);
-            handleWalletCreation(walletName, nickname, longPassword);
-            onCreateEthSuccess();
+            Eth eth = handleWalletCreation(walletName, nickname, longPassword);
+            onCreateEthSuccess(eth);
         } catch (Exception e) {
             e.printStackTrace();
             onCreateEthFail();
@@ -53,7 +58,7 @@ public class GenerateWalletService extends IntentService {
         super("GenerateWalletService");
     }
 
-    private void handleWalletCreation(String walletName, String nickname, String longPassword) throws Exception{
+    private Eth handleWalletCreation(String walletName, String nickname, String longPassword) throws Exception{
         File walletFile = WalletHelper.getWalletFile(this, walletName);
         Credentials credentials = WalletHelper.retrieveCredentials(walletFile, longPassword);
 
@@ -67,6 +72,7 @@ public class GenerateWalletService extends IntentService {
             .setIV(encryptedWallet.getIV());
 
         insertEth(eth);
+        return eth;
     }
 
 
@@ -78,8 +84,26 @@ public class GenerateWalletService extends IntentService {
     }
 
 
-    private void onCreateEthSuccess(){
+    private void onCreateEthSuccess(Eth eth){
         Log.i(TAG, "Eth created");
+        Intent viewEthIntent = new Intent(this, EthDetailActivity.class);
+        viewEthIntent.putExtra("eth", eth);
+
+        PendingIntent viewEthPendingIntent = PendingIntent.getActivity(
+                this,
+                1,
+                viewEthIntent,
+                PendingIntent.FLAG_NO_CREATE
+        );
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "")
+                .setContentTitle("Eth created")
+                .setContentText(eth.getNickname())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(viewEthPendingIntent);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(0, builder.build());
     }
 
     private void onCreateEthFail(){
