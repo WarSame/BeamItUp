4bd6f953ae0b43b8a20c50f9873ee81f2c4166bc
WarSame <graemecliffe@gmail.com>
2018-08-06 18:20:36 -0400

    Setting up CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,25 @@
+#!/usr/bin/env bash
 buildType='debug'
 
+#clean
+chmod +x gradlew
+./gradlew clean --stacktrace
+
+# build
+if [ $buildType = 'debug' ]; then
+	./gradlew assembleDebug --stacktrace
+elif [ $buildType = 'release' ]; then
+	./gradlew assembleRelease --stacktrace
+fi
+
+#test
 if [ $buildType = 'debug' ]; then
     ./gradlew testDebugUnitTest --stacktrace
 elif [ $buildType = 'release' ]; then
     ./gradlew testReleaseUnitTest --stacktrace
 fi
 
+#report
 mkdir report/test-results
 modules=("app" "common")
 for module in "${modules[@]}"
