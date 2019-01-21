cbffaa015f7f2377d50e4b0a47ef30d2ebd26753
WarSame <graemecliffe@gmail.com>
2019-01-21 09:44:01 -0500

    Working notifications unit test

--- app/src/androidTest/java/com/example/graeme/beamitup/NotificationsTest.java
+++ app/src/androidTest/java/com/example/graeme/beamitup/NotificationsTest.java
@@ -1,6 +1,7 @@
 package com.example.graeme.beamitup;
 
 import android.app.NotificationManager;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 
@@ -9,23 +10,29 @@ import com.example.graeme.beamitup.notifications.NotificationWrapper;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import static org.mockito.Mockito.RETURNS_SELF;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class NotificationsTest {
+    @Mock
     private NotificationCompat.Builder notificationBuilder;
+    @Mock
     private NotificationWrapper notificationWrapper;
+    @Mock
+    private NotificationManager notificationManager;
 
     @Before
     public void setUp(){
         NotificationBuilderProvider notificationBuilderProvider = Mockito.mock(NotificationBuilderProvider.class);
-        notificationBuilder = Mockito.mock(NotificationCompat.Builder.class, Mockito.RETURNS_SELF);
+        notificationBuilder = Mockito.mock(NotificationCompat.Builder.class, RETURNS_SELF);
 
         when(notificationBuilderProvider.get()).thenReturn(notificationBuilder);
 
-        NotificationManager notificationManager = Mockito.mock(NotificationManager.class);
+        notificationManager = Mockito.mock(NotificationManager.class);
 
         notificationWrapper = new NotificationWrapper(notificationBuilderProvider, notificationManager);
     }
@@ -34,7 +41,7 @@ public class NotificationsTest {
     public void createNotification_ValuesShouldMatch(){
         String title = "sometitle";
         String text = "sometext";
-        int color = 500;
+        int color = Color.YELLOW;
         long[] pattern = new long[]{0, 1, 2, 3, 100};
         String tag = "sometag";
 
@@ -47,7 +54,15 @@ public class NotificationsTest {
         );
 
         verify(notificationBuilder)
-                .setContentTitle(title)
+                .setContentTitle(title);
+        verify(notificationBuilder)
                 .setContentText(text);
+        verify(notificationBuilder)
+                .setColor(color);
+        verify(notificationBuilder)
+                .setVibrate(pattern);
+
+        //verify(notificationManager)
+        //        .notify(tag, 1, null);
     }
 }
